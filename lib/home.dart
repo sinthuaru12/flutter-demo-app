912b712889102a9c8c848f14af99b8f28168fe75
Sinthuja Arumainayagam <sinthuja@codegen.net>
2023-11-29 12:46:58 +0530

    flutter with Rest API

---
+++ lib/home.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rest API'),
+      ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final name = user['name']['first'];
+            final email = user['email'];
+            final image = user['picture']['thumbnail'];
+            return ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(image),
+                ),
+                title: Text(name),
+                subtitle: Text(email));
+          }),
+    );
+  }
+
+  Future<void> fetchUsers() async {
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+  }
+}
